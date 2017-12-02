6b0936d12286be817e7caed81dafeba39e3b5b0b
thechitesh <thechitesh@gmail.com>
2017-12-02 11:43:36 +0100

    Commiting for Service components

--- src/main/java/com/demo/controller/DemoRestService.java
+++ src/main/java/com/demo/controller/DemoRestService.java
@@ -1,14 +1,23 @@
 package com.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.services.DemoService;
 
 @RestController
 public class DemoRestService {
 	
+	@Autowired
+	private DemoService service;
+	
 	@RequestMapping("/test")
 	public String test() {
 		System.out.println("inside test");
+		
+		String value = service.testService();
+		System.out.println(value);
 		return "Hello Lipsa, Spring Boot is working";
 	}
 
--- src/main/java/com/demo/services/DemoService.java
+++ src/main/java/com/demo/services/DemoService.java
@@ -1,5 +1,13 @@
 package com.demo.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class DemoService {
+	
+	
+	public String testService() {
+		return "Returning from service";
+	}
 
 }
